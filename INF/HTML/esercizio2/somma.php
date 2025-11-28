4eadb3de8c814038c3838199468ada4edfd4b543
shamalwe <158327803+shamalwe@users.noreply.github.com>
2025-11-28 11:42:28 +0000

    up

--- INF/HTML/esercizio2/somma.php
+++ INF/HTML/esercizio2/somma.php
@@ -1,8 +1,34 @@
-<?php
+S<?php
 
-$A= $_POST['primonumero'];
+$A = $_POST['primonumero'];
 $B = $_POST['secondonumero'];
+$operazione = $_POST['operazione'];
 
-$somma = $A + $B;
-echo "La somma tra $A e $B è uguale a: $somma";
+switch ($operazione) 
+{
+	case 'somma':
+		$b = $A + $B;
+		echo "somma di $A e $B è uguale a $risultato";
+		break;
+	case 'differenza':
+		$risultato = $A - $B;
+		echo "differenza di $A e $B è uguale a $risultato";
+		break;
+	case 'prodotto':
+		$risultato = $A * $B;
+		echo "prodotto di $A e $B è uguale a $risultato";
+		break;
+	case 'divisione':
+		if ($B == 0) {
+			echo "Errore: divisione per zero.";
+		} 
+        else {
+			$risultato = $A / $B;
+			echo "divisione di $A per $B è uguale a $risultato";
+		}
+		break;
+	default:
+		echo "Operazione non valida.";
+}
+
 ?>
